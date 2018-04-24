5b28ba49d0d36f2a4694f3cf6a7c01ccdc5752cc
100Rashmi <singhrashmi579@gmail.com>
2018-04-24 22:50:03 +0530

    change table name

--- controllers/shortend_url_controller.py
+++ controllers/shortend_url_controller.py
@@ -4,11 +4,11 @@ from commons import constants
 from commons.constants import URL_PREFIX
 from commons.utils import convert_To_base_64, Logger
 from db.db_connection import db_connection
-from db.models import ShortendUrl
+from db.models import UrlInfo
 
 
 def create_shortend_url(long_url):
-    shorturl = ShortendUrl()
+    shorturl = UrlInfo()
     shorturl.long_url = long_url
     shorturl.created_time = datetime.utcnow()
     shorturl.status = constants.Status.INITIALIZE
@@ -23,9 +23,9 @@ def create_shortend_url(long_url):
     encoded_path = str(convert_To_base_64(url_id))
 
     updated_time = datetime.utcnow()
-    updated_response = db_session.query(ShortendUrl).filter(ShortendUrl.url_id == url_id).update({ShortendUrl.status:constants.Status.ACTIVE,
-                                                                                                  ShortendUrl.short_url: encoded_path,
-                                                                                                  ShortendUrl.updated_time: updated_time})
+    updated_response = db_session.query(UrlInfo).filter(UrlInfo.url_id == url_id).update({UrlInfo.status:constants.Status.ACTIVE,
+                                                                                          UrlInfo.short_url: encoded_path,
+                                                                                          UrlInfo.updated_time: updated_time})
     db_connection().commit()
 
     shorturl = URL_PREFIX+encoded_path
@@ -37,18 +37,18 @@ def redirect_url(shortpath):
 
     try:
         updated_time = datetime.utcnow()
-        updated_count = db_session.query(ShortendUrl).filter(ShortendUrl.short_url == shortpath,
-                                                             ShortendUrl.status == constants.Status.ACTIVE) \
-            .update({ShortendUrl.status: constants.Status.EXPIRED,ShortendUrl.updated_time: updated_time})
+        updated_count = db_session.query(UrlInfo).filter(UrlInfo.short_url == shortpath,
+                                                         UrlInfo.status == constants.Status.ACTIVE) \
+            .update({UrlInfo.status: constants.Status.EXPIRED, UrlInfo.updated_time: updated_time})
         db_connection().commit()
 
         # case: when two user hit same time for same shortend url
         if updated_count > 0:
-            url_info = db_session.query(ShortendUrl).filter(ShortendUrl.short_url == shortpath).first()
+            url_info = db_session.query(UrlInfo).filter(UrlInfo.short_url == shortpath).first()
             return constants.Status.SUCCESS, url_info.long_url
 
         else:
-            url_info_count = db_session.query(ShortendUrl).filter(ShortendUrl.short_url == shortpath).count()
+            url_info_count = db_session.query(UrlInfo).filter(UrlInfo.short_url == shortpath).count()
             if url_info_count:
                 return constants.Status.EXPIRED, 0
             else:
--- db/models.py
+++ db/models.py
@@ -4,14 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class ShortendUrl(Base):
-    __tablename__ = 'shortend_url'
+class UrlInfo(Base):
+    __tablename__ = 'urlinfo'
     url_id = Column(Integer, autoincrement=True, primary_key=True)
-    status = Column(String(30))
-    long_url = Column(String(2500))
+    status = Column(String(32))
+    long_url = Column(String(2048))
     created_time = Column(DateTime)
     updated_time = Column(DateTime)
-    short_url = Column(String(10), unique=True)
+    short_url = Column(String(32), unique=True)
 
 
 
