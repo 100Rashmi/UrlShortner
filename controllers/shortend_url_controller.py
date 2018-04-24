ff22d92dc089308ddf193aff48e0870f4d00e96c
100Rashmi <singhrashmi579@gmail.com>
2018-04-24 23:10:25 +0530

    add documentation and comment

--- controllers/shortend_url_controller.py
+++ controllers/shortend_url_controller.py
@@ -8,6 +8,15 @@ from db.models import UrlInfo
 
 
 def create_shortend_url(long_url):
+    '''
+    Create Short Url from Long Url
+    '''
+    '''
+    Here, the autoincrement id of the entry is taken and an offset is added to it, 
+    and the resultant is encoded to base 64
+    When the entry is made initially, the status is 'INITIALIZE'
+    After the encoded url is updated, the status is made 'ACTIVE' . 
+    '''
     shorturl = UrlInfo()
     shorturl.long_url = long_url
     shorturl.created_time = datetime.utcnow()
@@ -23,23 +32,41 @@ def create_shortend_url(long_url):
     encoded_path = str(convert_To_base_64(url_id))
 
     updated_time = datetime.utcnow()
-    updated_response = db_session.query(UrlInfo).filter(UrlInfo.url_id == url_id).update({UrlInfo.status:constants.Status.ACTIVE,
-                                                                                          UrlInfo.short_url: encoded_path,
-                                                                                          UrlInfo.updated_time: updated_time})
+    updated_response = db_session.query(UrlInfo).filter(UrlInfo.url_id == url_id).update(
+        {UrlInfo.status: constants.Status.ACTIVE,
+         UrlInfo.short_url: encoded_path,
+         UrlInfo.updated_time: updated_time})
     db_connection().commit()
 
-    shorturl = URL_PREFIX+encoded_path
+    shorturl = URL_PREFIX + encoded_path
     return shorturl
 
 
 def redirect_url(shortpath):
+    '''
+    Redirect Url only if it is the first request trying to access it.
+    '''
     db_session = db_connection().get_session()
 
     try:
         updated_time = datetime.utcnow()
-        updated_count = db_session.query(UrlInfo).filter(UrlInfo.short_url == shortpath,
-                                                         UrlInfo.status == constants.Status.ACTIVE) \
-            .update({UrlInfo.status: constants.Status.EXPIRED, UrlInfo.updated_time: updated_time})
+        '''
+            Trying to update the status of the entry with the above shorturl to 'EXPIRED' only 
+            if the current status is 'ACTIVE'.
+            
+            If the update is successful, 
+                Only then return the long url.
+            Else
+                Error Out
+            
+            The update is transactional and it will happen only once, because the status will no longer be 'ACTIVE'.     
+            This will ensure that only 1 request is able to access the long url from the short url. 
+        '''
+        updated_count = db_session.query(UrlInfo) \
+            .filter(UrlInfo.short_url == shortpath,
+                    UrlInfo.status == constants.Status.ACTIVE) \
+            .update({UrlInfo.status: constants.Status.EXPIRED,
+                     UrlInfo.updated_time: updated_time})
         db_connection().commit()
 
         # case: when two user hit same time for same shortend url
